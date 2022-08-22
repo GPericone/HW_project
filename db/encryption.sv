84a33e4e91425eeeda10087d1ff31398d813ada2
Robert0995 <111365149+Robert0995@users.noreply.github.com>
2022-08-22 18:45:58 +0200

    revisione e modifica codice per RC

--- db/encryption.sv
+++ db/encryption.sv
@@ -1,52 +1,73 @@
 /*C[i]=(P[i]-Pk)mod p*/
-module encryption(clk,rst_n,Plaintext,Public_key,mode,Char_ciphertext,C_ready);
-input              clk, rst_n; 
-input         [7:0] Plaintext;
-input         [7:0] Public_key;
-output        [7:0] Char_ciphertext;
-output              C_ready;
+`define NULL_CHAR 8'h00
 
-reg [7:0] CHAR_CIPHERTEXT;  assign Char_ciphertext = CHAR_CIPHERTEXT;
-reg C_READY;                assign C_ready = C_READY;
-reg [7:0] PUBLIC_KEY;
-reg [7:0] PLAINTEXT;
-reg [7:0] COMPUTE;
+module encryption(
+     input  clk
+    ,input rst_n 
+    ,input  [1:0] mode   // 2'b10 è la modalità per decifrare
+    ,input  [7:0] Plaintext
+    ,input  [7:0] Public_key
+    ,output [7:0] Char_ciphertext
+    ,output reg C_ready
+    ,output reg err_invalid_ptxt
+);
+//---------------------------------------------------------------------------
+// VARIABLES
+//---------------------------------------------------------------------------
 
+localparam LOWERCASE_A_CHAR = 8'h61;
+localparam LOWERCASE_Z_CHAR = 8'h7A;
+localparam P = 8'd227;
 
-reg [1:0] STAR;               assign S0=0, S1=1, S2=2;
+reg[7:0] sub;  // va da -227 a +256
+reg[7:0] result;
+reg tmp_C_ready;
 
-p_par = 8'B11100011;/*227*/
-C_ready = 0;
+//---------------------------------------------------------------------------
+// LOGIC DESIGN
+//---------------------------------------------------------------------------
+assign err_invalid_ptxt =  (Plaintext >= LOWERCASE_A_CHAR) &&
+                            (Plaintext <= LOWERCASE_Z_CHAR);
+p_par = 8'b11100011;/*227*/
 
-always @(rst_n==0)#1    begin  
-                            STAR <= S0; 
-                            C_READY <= 0;
-                            COMPUTE <= 8'B00000000;
-                            CHAR_CIPHERTEXT <= 8'B00000000;
-                            PUBLIC_KEY <= 8'B00000000;
-                            PLAINTEXT <= 8'B00000000;
-                        end
-always @ (posedge clk) if (rst_n == 1 & mode == 2'B10) #3
-    casex (STAR)
-        S0: begin PUBLIC_KEY <= Public_key; PLAINTEXT <= Plaintext; STAR <= (Public_key == 8'B00000000 || Plaintext == 8'B00000000)? S0 : S1; end
-        S1: begin COMPUTE <= (PLAINTEXT[7:0] - PUBLIC_KEY[7:0]); STAR <= S2; end
-        S2: begin CHAR_CIPHERTEXT <= (COMPUTE [7:0] - p_par); C_READY <=1; end      /*qui faccio il modulo grazie alla sottrazione*/
-    endcase
-
-endmodule
-
-
-
-
-/*
-always @ (*)
-// xP controllo se effettivamente ho una chiave pub. nuova
-// Xm guardare Pub_key_gen.sv per variabili mancanti, visto che questo pezzo di codice va unito all'altro
-if(!P_K_ready)begin
-    Char_ciphertext = diff_encryption % p_par;
-    C_ready = 1; 
-else (
-    C_ready = 0;
-)
+always @ (*) begin
+    if(!err_invalid_ptxt && mode == 2'b10) begin
+        sub = Plaintext - Public_key;
+        if(sub >=8'b00011101  && sub < 8'b00000000) begin           //-227<= sub <0
+            result = sub + p_par;
+            tmp_C_ready = 1'b1;
+        end
+        else if (sub >=8'b11100100 && sub <= 8'b11111111 ) begin     // 228<= sub <=256
+            result = sub - p_par;
+            tmp_C_ready = 1'b1;
+        end
+        else if (sub >=8'b00000000 && sub <= 8'b11100011 ) begin     // 0 <= sub <= 227
+            result = sub;
+            tmp_C_ready = 1'b1;
+        end
+    end
+    else begin
+        result = 8'b00000000;
+        sub = 8'b00000000;
+        err_invalid_ptxt = 1'b0;
+        Char_ciphertext = `NULL_CHAR;
+        tmp_C_ready = 1'b0;
+    end
 end
-*/
+
+always @(posedge clk or negedge rst_n) begin
+	if(!rst_n) begin
+		C_ready <= 1'b0;
+		Char_ciphertext <= `NULL_CHAR;
+	end
+    else begin
+		if(tmp_C_ready && !err_invalid_ptxt) begin
+			C_ready <= 1'b1;
+		end
+		else begin
+			C_ready <= 1'b0;
+			Char_ciphertext <= `NULL_CHAR;
+		end
+	end
+end
+endmodule
--- db/pub_key_gen.sv
+++ db/pub_key_gen.sv
@@ -1,77 +1,72 @@
 /*Pk=(Sk+q)modp*/
-module encryption(clk,rst_n,Q_par,Secret_key,Public_key,P_K_ready);
-input              clk, rst_n;
-input         [7:0] Q_par;
-input         [7:0] Secret_key;
-output        [7:0] Public_key;
-output              P_K_ready;
+`define NULL_CHAR 8'h00
 
-reg [7:0] PUBLIC_KEY;  assign Public_key = PUBLIC_KEY;
-reg P_K_READY;         assign P_K_ready = P_K_READY;
+module public_key_gen(
+     input  clk
+    ,input rst_n 
+    ,input  [1:0] mode   // 2'b10 è la modalità per decifrare
+    ,input  [7:0] Secret_key
+    ,output reg [7:0] Public_key
+    ,output reg P_K_ready
+    ,output err_invalid_seckey
+);
+//---------------------------------------------------------------------------
+// VARIABLES
+//---------------------------------------------------------------------------
 
-reg [7:0] SECRET_KEY;
-reg [7:0] Q_PAR;
-reg [8:0] COMPUTE;
+localparam P = 8'd227;
+localparam Q = 8'd225;
 
-reg [1:0] STAR;               assign S0=0, S1=1, S2=2;
-
-p_par = 8'B11100011;/*227*/
-P_K_ready = 0;
-
-always @(rst_n==0)#1    begin  
-                            STAR <= S0; 
-                            P_K_READY <= 0;
-                            COMPUTE <= 9'B000000000;
-                            PUBLIC_KEY <= 8'B00000000;
-                            SECRET_KEY <= 8'B00000000;
-                            Q_PAR <= 8'B00000000;
-                        end
-always @ (posedge clk) if (rst_n == 1 & mode == 2'B01) #3
-    casex (STAR)
-        S0: begin SECRET_KEY <= Secret_key; Q_PAR <= Q_par; STAR <= (Secret_key == 8'B00000000 || Q_par == 8'B00000000)? S0 : S1; end
-        S1: begin COMPUTE <= (SECRET_KEY[7:0] + Q_par[7:0]); STAR <= S2; end
-        S2: begin PUBLIC_KEY <= (COMPUTE [8:0] - p_par); STAR <=S3; end  /*qui faccio il modulo grazie alla sottrazione*/
-        S3: begin PUBLIC_KEY <= PUBLIC_KEY[7:0]; P_K_READY <=1; end   /* riporto public_key su 8 bit  */
-
-    endcase
-
-endmodule
+reg [8:0] sum;  // va da 1 a 451
+reg [8:0] result; // va da 1 a 224
+reg tmp_P_K_ready;
+reg [7:0] tmp_publicKey;
 
 
+//---------------------------------------------------------------------------
+// LOGIC DESIGN
+//---------------------------------------------------------------------------
 
-  /*
-module key_pair_generation(
-    input            clk
-  ,input            rst_n
-  ,input      [7:0] q_par
-  ,input      [7:0] secret_key
-  ,input      [1:0] mode
-  ,input      [7:0] p_par
-  ,output reg       P_K_ready
-  ,output reg [7:0] Public_key
-)
+assign err_invalid_seckey = secret_key < 1 || secret_key > P - 1;
+p_par = 9'b011100011;/*227*/
+p_max = 9'b111000011; /*451*/
+always @ (*) begin
 
-wire somma_key_pair_generation; 
+    if(!err_invalid_seckey && mode == 2'b01) begin
+        sum = ciphertext +secret_key + Q;
+        if(sum >= 9'b000000001 && sum <=p_par) begin
+            result = sum;
+            tmp_P_K_ready = 1'b1;
+        end
+        else if(sum > 9'b000000001 && sum <=p_max) begin
+            result = sum - p_par;
+            tmp_P_K_ready = 1'b1;
+        end
+        tmp_publicKey = result[7:0];
+    end
+    else begin
+        result = 9'b000000000;
+        sum = 9'b000000000;
+        err_invalid_seckey= 1'b0;
+        tmp_publicKey = `NULL_CHAR;
+        tmp_P_K_ready = 1'b0;
+    end
+end
 
-assign somma_key_pair_generation = secret_key + q_par;
-q_par = 225; 
-p_par = 227;
-P_K_ready = 0;
-
-always @ (*)
-if(secret_key > 1 && secret_key < (p_par - 1))begin
-    Public_key = somma_key_pair_generation % p_par;
-    P_K_ready = 1;
-else (
-    P_K_ready = 0;
-)
+always @(posedge clk or negedge rst_n) begin
+	if(!rst_n) begin
+		P_K_ready <= 1'b0;
+		Public_key <= `NULL_CHAR;
+	end
+    else begin
+		if(tmp_P_K_ready && !err_invalid_seckey) begin
+			P_K_ready <= 1'b1;
+			Public_key <= tmp_publicKey;
+		end
+		else begin
+			P_K_ready <= 1'b0;
+		    Public_key <= `NULL_CHAR;
+		end
+	end
 end
 endmodule
-*/
-
-
-
-
-
-
-
