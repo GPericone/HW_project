f85134882ea7afc6ba726351800e7c3e22f44927
Robert0995 <111365149+Robert0995@users.noreply.github.com>
2022-08-23 14:20:30 +0200

    testbench creati

---
+++ tb/tb_encryption.sv
@@ -0,0 +1,49 @@
+// -----------------------------------------------------------------------------
+// ---- Testbench for file encryption
+// -----------------------------------------------------------------------------
+
+module encryption;
+
+    reg clk = 1'b0;
+    always #5 clk = !clk;
+
+    reg rst_n = 1'b0;
+
+    localparam NUL_CHAR = 8'h00;
+
+    reg [1:0] mode = 2'b10;
+    reg [7:0] Plaintext = NUL_CHAR; 
+    reg [7:0] Public_key = NUL_CHAR;
+    wire [7:0] Char_ciphertext;
+    wire err_invalid_ptxt;
+    wire  C_ready;
+
+
+
+    public_key_gen sae_dec(
+     .clk                       (clk)
+    ,.rst_n                     (rst_n)
+    ,.mode                      (mode)
+    ,.Plaintext                 (Plaintext)
+    ,.Public_key                (Public_key)
+    ,.Char_ciphertext           (Char_ciphertext)
+    ,.C_ready                   (C_ready)
+    ,.err_invalid_ptxt          (err_invalid_ptxt)
+    );
+
+
+    initial begin
+        // Test iniziale, tutti i dati corretti
+        @(posedge clk);
+        Public_key = 8'hc8; // 200
+        Plaintext = 8'h7F; // ciphertext di b
+
+        @(posedge clk);
+        @(posedge clk);		
+        $display("CIPHERTEXT: %c -PUBLIC KEY: %h - PLAINTEXT: %c", Char_ciphertext, Public_key, Plaintext);
+
+        $stop;
+
+    end
+
+endmodule
---
+++ tb/tb_pub_key_gen.sv
@@ -0,0 +1,44 @@
+// -----------------------------------------------------------------------------
+// ---- Testbench for file pub_key_generator
+// -----------------------------------------------------------------------------
+
+module pub_key_gen_tb_checks;
+
+    reg clk = 1'b0;
+    always #5 clk = !clk;
+
+    reg rst_n = 1'b0;
+
+    localparam NUL_CHAR = 8'h00;
+
+    reg [1:0] mode = 2'b01;
+    reg [7:0] Secret_key = NUL_CHAR; 
+    wire [7:0] Public_key;
+    wire err_invalid_seckey;
+    wire  P_K_ready;
+
+    public_key_gen sae_dec(
+     .clk                       (clk)
+    ,.rst_n                     (rst_n)
+    ,.mode                      (mode)
+    ,.Secret_key                (Secret_key)
+    ,.Public_key                (Public_key)
+    ,.P_K_ready                 (P_K_ready)
+    ,.err_invalid_seckey        (err_invalid_seckey)
+    );
+
+
+    initial begin
+        // Test iniziale, tutti i dati corretti
+        @(posedge clk);
+        secret_key = 8'hc8; // 200
+
+        @(posedge clk);
+        @(posedge clk);		
+        $display("SECRET KEY: %h - PUBLICKEY: %h", secret_key, Public_key);
+
+        $stop;
+
+    end
+
+endmodule
