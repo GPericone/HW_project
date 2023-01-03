dfd42d48e1b24bf5f229669700d9e6344b675534
GPericone <46968685+GPericone@users.noreply.github.com>
2023-01-03 21:23:18 +0100

    Update sae_tb.sv
    Full documentation

--- tb/sae_tb.sv
+++ tb/sae_tb.sv
@@ -60,15 +60,27 @@ module sae_tb_checks;
     );
 
 
-    int FILE; 
-    reg [7:0] PTXT_W [$];
-    reg [7:0] CTXT_W [$];
-    reg [7:0] PTXT_J [$];
-    reg [7:0] CTXT_J [$];
+    
+    int FILE;               // variable to save the return value of open()
+    reg [7:0] PTXT_W [$];   // variable to save Walt's plaintext
+    reg [7:0] CTXT_W [$];   // variable to save Walt's ciphertext
+    reg [7:0] PTXT_J [$];   // variable to save Jesse's plaintext
+    reg [7:0] CTXT_J [$];   // variable to save Jesse's ciphertext
+
+    // Support variables used as the argument of scanf()
     string char_w;
     string char_w2;
     string char_j;
     string char_j2;
+
+    /*
+    The testbench will use write and read operations from files to simulate the exchange of data 
+    across the two modules. Specifically:
+    - Walt's public key will be saved in the publickey_j.txt file, to simulate sending the key to Jesse.
+    - Jesse's public key will be saved in the publickey_w.txt file, to simulate sending the key to Walt.
+    - The ciphertext produced by Walt by encrypting his own plaintext will be saved in the ciphertext_j.txt file, 
+    to simulate sending the ciphertext to Jesse.
+    */
 
 	initial begin
         @(posedge rst_n);
@@ -78,6 +90,7 @@ module sae_tb_checks;
         //-------------------------------------------------------------------
         
         @(posedge clk);
+        // Walt opens the file containing his private key, if the opening fails the execution is aborted.
         FILE = $fopen("tv/privatekey_w.txt", "rb");
         if (FILE) begin
             $display("File privatekey_w was opened successfully : %0d", FILE);
@@ -86,6 +99,7 @@ module sae_tb_checks;
             $display("File privatekey_w was NOT opened successfully : %0d", FILE);
             $finish;
         end
+        // Walt reads his own private key from the file, if the read fails the execution is aborted.
         if($fscanf(FILE, "%b", key_input_w) == 1)begin
             $display("Walt's private key was successfully loaded");
         end
@@ -93,13 +107,25 @@ module sae_tb_checks;
             $display("Walt's private key was NOT loaded correctly");
             $finish;
         end
+        // Walt closes the previously opened file
         $fclose(FILE);
 
+        // Inputs are set for the public key generation operation to be carried out.
         mode_w = 2'b01;
         data_input_w = 8'd0;
         inputs_valid_w = 1'b1;
+
+        /* 
+        Inputs_valid is set to 0 so as to signal that it is possible to operate on the data sampled
+        from the registers and not to sample the next input values.
+        */
         @(posedge clk);
         inputs_valid_w = 1'b0;
+
+        /*
+        Execution continues waiting for output_ready_w to go to 1, 
+        while checking that the noncompliant private key error does not go to 1.
+        */
         @(posedge clk);
         #3 while (output_ready_w != 1'b1) begin
             if(err_invalid_seckey_w == 1'b1) begin
@@ -109,6 +135,8 @@ module sae_tb_checks;
             $display("Walt's public key is NOT yet ready");
         end
         $display("Walt's public key was generated");
+
+        // Walt opens the file that will contain his public key, if the opening fails the execution is aborted.
         FILE = $fopen("tv/publickey_j.txt", "wb");
         if (FILE) begin
             $display("File publickey_j was opened successfully : %0d", FILE);
@@ -118,9 +146,11 @@ module sae_tb_checks;
             $finish;
         end
 
+        // Walt writes his own public key inside the file, then closes it.
         $fdisplay(FILE, "%b", data_output_w);
         $fclose(FILE);
 
+        // mode_w is set to 00 (no action) to prevent module operations from continuing.
         mode_w = 2'b00;
         @(posedge clk);
 
@@ -128,6 +158,7 @@ module sae_tb_checks;
         // JESSE GENERATES THE PUBLIC KEY
         //-------------------------------------------------------------------
 
+        // Jesse opens the file containing his private key, if the opening fails the execution is aborted.
         FILE = $fopen("tv/privatekey_j.txt", "rb");
         if (FILE) begin
             $display("File privatekey_j was opened successfully : %0d", FILE);
@@ -136,6 +167,7 @@ module sae_tb_checks;
             $display("File privatekey_j was NOT opened successfully : %0d", FILE);
             $finish;
         end  
+        // Jesse reads his own private key from the file, if the read fails the execution is aborted.
         if($fscanf(FILE, "%b", key_input_j) == 1) begin
             $display("Jesse's private key was successfully loaded");
         end
@@ -143,13 +175,25 @@ module sae_tb_checks;
             $display("Jesse's private key was NOT loaded correctly");
             $finish;
         end
+        // Jesse closes the previously opened file
         $fclose(FILE);
 
+        // Inputs are set for the public key generation operation to be carried out.
         mode_j = 2'b01;
         data_input_j = 8'd0;
         inputs_valid_j = 1'b1;
+
+        /* 
+        Inputs_valid is set to 0 so as to signal that it is possible to operate on the data sampled
+        from the registers and not to sample the next input values.
+        */
         @(posedge clk);
         inputs_valid_j = 1'b0;
+
+        /*
+        Execution continues waiting for output_ready_j to go to 1, 
+        while checking that the noncompliant private key error does not go to 1.
+        */
         @(posedge clk);
         #3 while (output_ready_j != 1'b1) begin
             if(err_invalid_seckey_j == 1'b1) begin
@@ -159,6 +203,8 @@ module sae_tb_checks;
             $display("Jesse's public key is NOT yet ready");
         end
         $display("Jesse's public key was generated");
+
+        // Jesse opens the file that will contain his public key, if the opening fails the execution is aborted.
         FILE = $fopen("tv/publickey_w.txt", "wb");
         if (FILE) begin
             $display("File publickey_w was opened successfully : %0d", FILE);
@@ -167,9 +213,12 @@ module sae_tb_checks;
             $display("File publickey_w was NOT opened successfully : %0d", FILE);
             $finish;
         end 
+
+        // Jesse writes his own public key inside the file, then closes it.
         $fdisplay(FILE, "%b", data_output_j);
         $fclose(FILE);
 
+        // mode_w is set to 00 (no action) to prevent module operations from continuing.
         mode_j = 2'b00;
         @(posedge clk);
 
@@ -177,6 +226,7 @@ module sae_tb_checks;
         // WALT ENCRYPTS THE PLAINTEXT
         //-------------------------------------------------------------------
 
+        // Walt opens the file containing Jesse's public key, if the opening fails the execution is aborted.
         FILE = $fopen("tv/publickey_w.txt", "rb");
         if (FILE)  begin
             $display("File publickey_w was opened successfully : %0d", FILE);
@@ -185,6 +235,8 @@ module sae_tb_checks;
             $display("File publickey_w was NOT opened successfully : %0d", FILE);
             $finish;
         end
+
+        // Walt reads Jesse's public key from the file, if the read fails the execution is aborted.
         if($fscanf(FILE, "%b", key_input_w) == 1) begin
             $display("Jesse's public key was successfully loaded");
         end
@@ -194,6 +246,7 @@ module sae_tb_checks;
         end
         $fclose(FILE);
 
+        // Walt opens the file containing his own plaintext, if the opening fails the execution is aborted.
         FILE = $fopen("tv/plaintext_w.txt", "r");
         if (FILE)  begin
             $display("File plaintext_w was opened successfully : %0d", FILE);
@@ -202,23 +255,34 @@ module sae_tb_checks;
             $display("File plaintext_w was NOT opened successfully : %0d", FILE);
             $finish;
         end
+        // For each plaintext character, the inputs for the encryption operation are set.
         while($fscanf(FILE, "%c", char_w) == 1) begin
             data_input_w = int'(char_w);
             mode_w = 2'b10;
             inputs_valid_w = 1'b1;
             @(posedge clk);
+            /* 
+            Inputs_valid is set to 0 so as to signal that it is possible to operate on the data sampled
+            from the registers and not to sample the next input values.
+            */
             inputs_valid_w = 1'b0;
             @(posedge clk);
+             /*
+            Execution continues waiting for output_ready_w to go to 1, 
+            while checking that the noncompliant plaintext character error does not go to 1.
+            */
             #3 while(output_ready_w != 1'b1) begin
                 if(err_invalid_ptxt_char_w == 1'b1) begin
                     $display("An invalid character was inserted in the plaintext");
                     $finish;
                 end
             end
+            // The character ciphertext has been computed and is inserted into the queue.
             CTXT_W.push_back(data_output_w);
         end
         $fclose(FILE);
 
+        // Walt opens the file that will contain the ciphertext, if the opening fails the execution is aborted.
         FILE = $fopen("tv/ciphertext_j.txt", "w");
         if (FILE) begin
             $display("File ciphertext_j was opened successfully : %0d", FILE);
@@ -228,11 +292,13 @@ module sae_tb_checks;
             $finish;
         end
 
+        // Walt writes the ciphertext inside the file, then closes it.
         foreach(CTXT_W[i]) begin
             $fwrite(FILE, "%c", CTXT_W[i]);
         end
         $fclose(FILE);
 
+        // mode_w is set to 00 (no action) to prevent module operations from continuing. 
         mode_w = 2'b00;
         @(posedge clk);
 
@@ -240,6 +306,7 @@ module sae_tb_checks;
         // JESSE DECRYPTS THE CIPHERTEXT
         //-------------------------------------------------------------------
 
+        // Jesse opens the file containing his own private key, if the opening fails the execution is aborted.
         FILE = $fopen("tv/privatekey_j.txt", "rb");
         if (FILE) begin
             $display("File privatekey_j was opened successfully : %0d", FILE);
@@ -248,6 +315,8 @@ module sae_tb_checks;
             $display("File privatekey_j was NOT opened successfully : %0d", FILE);
             $finish;
         end
+
+        // Jesse reads his own private key from the file, if the read fails the execution is aborted.
         if($fscanf(FILE, "%b", key_input_j) == 1) begin
             $display("Jesse's private key was successfully loaded");
         end
@@ -255,8 +324,10 @@ module sae_tb_checks;
             $display("Jesse's private key was NOT loaded correctly");
             $finish;
         end
+        // Jesse closes the previously opened file
         $fclose(FILE);
 
+        // Jesse opens the file containing the ciphertext, if the opening fails the execution is aborted.
         FILE = $fopen("tv/ciphertext_j.txt", "r");
         if (FILE) begin
             $display("File ciphertext_j was opened successfully : %0d", FILE);
@@ -265,23 +336,35 @@ module sae_tb_checks;
             $display("File ciphertext_j was NOT opened successfully : %0d", FILE);
             $finish;
         end
+
+        // For each ciphertext character, the inputs for the decryption operation are set.
         while($fscanf(FILE, "%c", char_j) == 1) begin
             data_input_j = int'(char_j);
             mode_j = 2'b11;
             inputs_valid_j = 1'b1;
             @(posedge clk);
+            /* 
+            Inputs_valid is set to 0 so as to signal that it is possible to operate on the data sampled
+            from the registers and not to sample the next input values.
+            */
             inputs_valid_j = 1'b0;
             @(posedge clk);
+            /*
+            Execution continues waiting for output_ready_j to go to 1, 
+            while checking that the noncompliant ciphertext character error does not go to 1.
+            */
             #3 while(output_ready_j != 1'b1) begin
                 if(err_invalid_ctxt_char_j == 1'b1) begin
                     $display("An invalid character was inserted in the ciphertext");
                     $finish;
                 end
             end
+            // The character plaintext has been computed and is inserted into the queue.
             PTXT_J.push_back(data_output_j);
         end
         $fclose(FILE);
 
+        // Jesse opens the file that will contain the plaintext, if the opening fails the execution is aborted.
         FILE = $fopen("tv/plaintext_j.txt", "w");
         if (FILE) begin
             $display("File plaintext_j was opened successfully : %0d", FILE);
@@ -290,13 +373,17 @@ module sae_tb_checks;
             $display("File plaintext_j was NOT opened successfully : %0d", FILE);
             $finish;
         end
+
+        // Jesse writes the plaintext inside the file, then closes it.
         foreach(PTXT_J[i]) begin
             $fwrite(FILE, "%c", PTXT_J[i]);
         end
         $fclose(FILE);
 
+        // The queue containing the plaintext is emptied so that the plaintext obtained is not shown in clear.
         PTXT_J.delete();
 
+        // mode_w is set to 00 (no action) to prevent module operations from continuing.
         mode_j = 2'b00;
         @(posedge clk);
 
@@ -304,6 +391,7 @@ module sae_tb_checks;
         // I CHECK THAT THE ORIGINAL PLAINTEXT AND THE PLAINTEXT OBTAINED BY DECRYPTION MATCH
         //-------------------------------------------------------------------
 
+        // The file containing the initial plaintext is opened, if the opening fails the execution is aborted.
         FILE = $fopen("tv/plaintext_w.txt", "r");
         if (FILE) begin
             $display("File plaintext_w was opened successfully : %0d", FILE);
@@ -312,11 +400,14 @@ module sae_tb_checks;
             $display("File plaintext_w was NOT opened successfully : %0d", FILE);
             $finish;
         end
+
+        // The initial plaintext is saved within the PTXT_W queue.
         while($fscanf(FILE, "%c", char_w2) == 1) begin
             PTXT_W.push_back(int'(char_w2));
         end
         $fclose(FILE);
 
+        // The file containing the calculated plaintext is opened, if the opening fails the execution is aborted.
         FILE = $fopen("tv/plaintext_j.txt", "r");
         if (FILE) begin
             $display("File plaintext_j was opened successfully : %0d", FILE);
@@ -325,11 +416,14 @@ module sae_tb_checks;
             $display("File plaintext_j was NOT opened successfully : %0d", FILE);
             $finish;
         end
+
+        // The calculated plaintext is saved within the PTXT_J queue.
         while($fscanf(FILE, "%c", char_j2) == 1) begin
             PTXT_J.push_back(int'(char_j2));
         end
         $fclose(FILE);
 
+        // The two plaintexts are compared to verify that they are the same.
         if(PTXT_J == PTXT_W) begin
             $display("Plaintexts MATCH!");
         end
