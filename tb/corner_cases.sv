a6c4127971c243c4d6404bda6c47d56f0a2daccd
GPericone <46968685+GPericone@users.noreply.github.com>
2023-01-04 14:19:22 +0100

    Update corner_cases.sv
    Final version

--- tb/corner_cases.sv
+++ tb/corner_cases.sv
@@ -1,4 +1,4 @@
-module corner_cases_checks.sv;
+module corner_cases_checks;
 
     reg clk = 1'b0;
     always #5 clk = !clk;
@@ -53,7 +53,7 @@ module corner_cases_checks.sv;
         expected_err_invalid_ctxt_char = 1'b0;
 
         @(posedge clk);
-        inputs_valid_w = 1'b0;
+        inputs_valid = 1'b0;
 
         wait(output_ready == 1'b1);
         
@@ -69,8 +69,12 @@ module corner_cases_checks.sv;
         assert(err_invalid_ctxt_char == expected_err_invalid_ctxt_char)
           else $error("Test case 1: Unexpected err_invalid_ctxt_char value");
         $display("Test case 1: PASSED");
+        $display("Data output: %d - Expected data output: %d - Output ready: %d - Expected output ready: %d", 
+                  data_output, expected_data_output, output_ready, expected_output_ready);
+        $display("err_invalid_ptxt_char: %b - expected_err_invalid_ptxt_char: %b - err_invalid_seckey: %b - expected_err_invalid_seckey: %b - err_invalid_ctxt_char: %b - expected_err_invalid_ctxt_char: %b", 
+                  err_invalid_ptxt_char, expected_err_invalid_ptxt_char, err_invalid_seckey, expected_err_invalid_seckey, err_invalid_ctxt_char, expected_err_invalid_ctxt_char);
 
-        mode_w = 2'b00;
+        mode= 2'b00;
         @(posedge clk);
 
         // Test case 2: Generate public key with private key == 226
@@ -85,7 +89,7 @@ module corner_cases_checks.sv;
         expected_err_invalid_ctxt_char = 1'b0;
 
         @(posedge clk);
-        inputs_valid_w = 1'b0;
+        inputs_valid = 1'b0;
 
         wait(output_ready == 1'b1);
         
@@ -101,10 +105,13 @@ module corner_cases_checks.sv;
         assert(err_invalid_ctxt_char == expected_err_invalid_ctxt_char)
           else $error("Test case 2: Unexpected err_invalid_ctxt_char value");
         $display("Test case 2: PASSED");
+        $display("Data output: %d - Expected data output: %d - Output ready: %d - Expected output ready: %d", 
+                  data_output, expected_data_output, output_ready, expected_output_ready);
+        $display("err_invalid_ptxt_char: %b - expected_err_invalid_ptxt_char: %b - err_invalid_seckey: %b - expected_err_invalid_seckey: %b - err_invalid_ctxt_char: %b - expected_err_invalid_ctxt_char: %b", 
+                  err_invalid_ptxt_char, expected_err_invalid_ptxt_char, err_invalid_seckey, expected_err_invalid_seckey, err_invalid_ctxt_char, expected_err_invalid_ctxt_char);
 
-        mode_w = 2'b00;
+        mode = 2'b00;
         @(posedge clk);
-
 
         // Test case 3: Encrypt plaintext character "a"
         mode = 2'b10;
@@ -118,7 +125,7 @@ module corner_cases_checks.sv;
         expected_err_invalid_ctxt_char = 1'b0;
 
         @(posedge clk);
-        inputs_valid_w = 1'b0;
+        inputs_valid = 1'b0;
 
         wait(output_ready == 1'b1);
 
@@ -134,6 +141,13 @@ module corner_cases_checks.sv;
         assert(err_invalid_ctxt_char == expected_err_invalid_ctxt_char)
         else $error("Test case 3: Unexpected err_invalid_ctxt_char value");
         $display("Test case 3: PASSED");
+        $display("Data output: %c - Expected data output: %c - Output ready: %d - Expected output ready: %d", 
+                  data_output, expected_data_output, output_ready, expected_output_ready);
+        $display("err_invalid_ptxt_char: %b - expected_err_invalid_ptxt_char: %b - err_invalid_seckey: %b - expected_err_invalid_seckey: %b - err_invalid_ctxt_char: %b - expected_err_invalid_ctxt_char: %b", 
+                  err_invalid_ptxt_char, expected_err_invalid_ptxt_char, err_invalid_seckey, expected_err_invalid_seckey, err_invalid_ctxt_char, expected_err_invalid_ctxt_char);
+
+        mode = 2'b00;
+        @(posedge clk);
 
          // Test case 4: Encrypt plaintext character "z"
         mode = 2'b10;
@@ -147,7 +161,7 @@ module corner_cases_checks.sv;
         expected_err_invalid_ctxt_char = 1'b0;
 
         @(posedge clk);
-        inputs_valid_w = 1'b0;
+        inputs_valid = 1'b0;
 
         wait(output_ready == 1'b1);
 
@@ -163,6 +177,13 @@ module corner_cases_checks.sv;
         assert(err_invalid_ctxt_char == expected_err_invalid_ctxt_char)
           else $error("Test case 4: Unexpected err_invalid_ctxt_char value");
         $display("Test case 4: PASSED");
+        $display("Data output: %c - Expected data output: %c - Output ready: %d - Expected output ready: %d", 
+                  data_output, expected_data_output, output_ready, expected_output_ready);
+        $display("err_invalid_ptxt_char: %b - expected_err_invalid_ptxt_char: %b - err_invalid_seckey: %b - expected_err_invalid_seckey: %b - err_invalid_ctxt_char: %b - expected_err_invalid_ctxt_char: %b", 
+                  err_invalid_ptxt_char, expected_err_invalid_ptxt_char, err_invalid_seckey, expected_err_invalid_seckey, err_invalid_ctxt_char, expected_err_invalid_ctxt_char);
+
+        mode = 2'b00;
+        @(posedge clk);
 
          // Test case 5: Decrypt ciphertext of character "z"
         mode = 2'b11;
@@ -174,7 +195,11 @@ module corner_cases_checks.sv;
         expected_err_invalid_ptxt_char = 1'b0;
         expected_err_invalid_seckey = 1'b0;
         expected_err_invalid_ctxt_char = 1'b0;
-        #10;
+        
+        @(posedge clk);
+        inputs_valid = 1'b0;
+
+        wait(output_ready == 1'b1);
 
         // Check outputs of test case 5
         assert(data_output == expected_data_output)
@@ -188,8 +213,16 @@ module corner_cases_checks.sv;
         assert(err_invalid_ctxt_char == expected_err_invalid_ctxt_char)
           else $error("Test case 5: Unexpected err_invalid_ctxt_char value");
         $display("Test case 5: PASSED");
+        $display("Data output: %c - Expected data output: %c - Output ready: %d - Expected output ready: %d", 
+                  data_output, expected_data_output, output_ready, expected_output_ready);
+        $display("err_invalid_ptxt_char: %b - expected_err_invalid_ptxt_char: %b - err_invalid_seckey: %b - expected_err_invalid_seckey: %b - err_invalid_ctxt_char: %b - expected_err_invalid_ctxt_char: %b", 
+                  err_invalid_ptxt_char, expected_err_invalid_ptxt_char, err_invalid_seckey, expected_err_invalid_seckey, err_invalid_ctxt_char, expected_err_invalid_ctxt_char);
+
+        mode = 2'b00;
+        @(posedge clk);
 
         // Test case 6: Check invalid plaintext character
+        mode = 2'b10;
         key_input = 8'hE2;
         inputs_valid = 1'b1;
         expected_data_output = 8'h00;
@@ -199,7 +232,7 @@ module corner_cases_checks.sv;
         expected_err_invalid_ctxt_char = 1'b0;
         
         @(posedge clk);
-        inputs_valid_w = 1'b0;
+        inputs_valid = 1'b0;
 
         wait(output_ready == 1'b1 || err_invalid_ctxt_char == 1'b1 || err_invalid_ptxt_char == 1'b1 || err_invalid_seckey == 1'b1);
 
@@ -215,6 +248,85 @@ module corner_cases_checks.sv;
         assert(err_invalid_ctxt_char == expected_err_invalid_ctxt_char)
           else $error("Test case 6: Unexpected err_invalid_ctxt_char value");
         $display("Test case 6: PASSED");
+        $display("Data output: %c - Expected data output: %c - Output ready: %d - Expected output ready: %d", 
+                  data_output, expected_data_output, output_ready, expected_output_ready);
+        $display("err_invalid_ptxt_char: %b - expected_err_invalid_ptxt_char: %b - err_invalid_seckey: %b - expected_err_invalid_seckey: %b - err_invalid_ctxt_char: %b - expected_err_invalid_ctxt_char: %b", 
+                  err_invalid_ptxt_char, expected_err_invalid_ptxt_char, err_invalid_seckey, expected_err_invalid_seckey, err_invalid_ctxt_char, expected_err_invalid_ctxt_char);
+          
+        mode = 2'b00;
+        @(posedge clk);
+
+        // Test case 7: Check invalid secret key
+        mode = 2'b01;
+        data_input = 8'h00;
+        key_input = 8'hFF;
+        inputs_valid = 1'b1;
+        expected_data_output = 8'h00;
+        expected_output_ready = 1'b0;
+        expected_err_invalid_ptxt_char = 1'b0;
+        expected_err_invalid_seckey = 1'b1;
+        expected_err_invalid_ctxt_char = 1'b0;
+               
+        @(posedge clk);
+        inputs_valid = 1'b0;
+
+        wait(output_ready == 1'b1 || err_invalid_ctxt_char == 1'b1 || err_invalid_ptxt_char == 1'b1 || err_invalid_seckey == 1'b1);
+
+        // Check outputs of test case 7
+        assert(data_output == expected_data_output)
+          else $error("Test case 7: Unexpected data_output value");
+        assert(output_ready == expected_output_ready)
+          else $error("Test case 7: Unexpected output_ready value");
+        assert(err_invalid_ptxt_char == expected_err_invalid_ptxt_char)
+          else $error("Test case 7: Unexpected err_invalid_ptxt_char value");
+        assert(err_invalid_seckey == expected_err_invalid_seckey)
+          else $error("Test case 7: Unexpected err_invalid_seckey value");
+        assert(err_invalid_ctxt_char == expected_err_invalid_ctxt_char)
+          else $error("Test case 7: Unexpected err_invalid_ctxt_char value");
+        $display("Test case 7: PASSED");
+        $display("Data output: %d - Expected data output: %d - Output ready: %d - Expected output ready: %d", 
+                  data_output, expected_data_output, output_ready, expected_output_ready);
+        $display("err_invalid_ptxt_char: %b - expected_err_invalid_ptxt_char: %b - err_invalid_seckey: %b - expected_err_invalid_seckey: %b - err_invalid_ctxt_char: %b - expected_err_invalid_ctxt_char: %b", 
+                  err_invalid_ptxt_char, expected_err_invalid_ptxt_char, err_invalid_seckey, expected_err_invalid_seckey, err_invalid_ctxt_char, expected_err_invalid_ctxt_char);
+
+        mode = 2'b00;
+        @(posedge clk);
+
+        // Test case 8: Check invalid ciphertext character
+        mode = 2'b11;
+        data_input = 8'hFF;  // the decryption will give a plaintext with ASCII code of 25 (not valid)
+        key_input = 8'hE2;
+        inputs_valid = 1'b1;
+        expected_data_output = 8'h00;
+        expected_output_ready = 1'b0;
+        expected_err_invalid_ptxt_char = 1'b0;
+        expected_err_invalid_seckey = 1'b0;
+        expected_err_invalid_ctxt_char = 1'b1;
+
+        @(posedge clk);
+        inputs_valid = 1'b0;
+
+        wait(output_ready == 1'b1 || err_invalid_ctxt_char == 1'b1 || err_invalid_ptxt_char == 1'b1 || err_invalid_seckey == 1'b1);
+
+        // Check outputs of test case 8
+        assert(data_output == expected_data_output)
+          else $error("Test case 8: Unexpected data_output value");
+        assert(output_ready == expected_output_ready)
+          else $error("Test case 8: Unexpected output_ready value");
+        assert(err_invalid_ptxt_char == expected_err_invalid_ptxt_char)
+          else $error("Test case 8: Unexpected err_invalid_ptxt_char value");
+        assert(err_invalid_seckey == expected_err_invalid_seckey)
+          else $error("Test case 8: Unexpected err_invalid_seckey value");
+        assert(err_invalid_ctxt_char == expected_err_invalid_ctxt_char)
+          else $error("Test case 8: Unexpected err_invalid_ctxt_char value");
+        $display("Test case 8: PASSED");
+        $display("Data output: %c - Expected data output: %c - Output ready: %d - Expected output ready: %d", 
+                  data_output, expected_data_output, output_ready, expected_output_ready);
+        $display("err_invalid_ptxt_char: %b - expected_err_invalid_ptxt_char: %b - err_invalid_seckey: %b - expected_err_invalid_seckey: %b - err_invalid_ctxt_char: %b - expected_err_invalid_ctxt_char: %b", 
+                  err_invalid_ptxt_char, expected_err_invalid_ptxt_char, err_invalid_seckey, expected_err_invalid_seckey, err_invalid_ctxt_char, expected_err_invalid_ctxt_char);
+
+        mode = 2'b00;
+        @(posedge clk);
 
     end
    
